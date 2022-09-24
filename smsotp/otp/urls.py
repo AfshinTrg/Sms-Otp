165d59296631fff3574e39634a11919a16efe820
afshintrg <ataraghijoo@gmial.com>
2022-09-24 11:10:31 +0330

    Add RegisterNumber view (create and send new code)

--- smsotp/otp/urls.py
+++ smsotp/otp/urls.py
@@ -6,5 +6,6 @@ app_name = 'otp'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('register/', views.RegisterNumber.as_view(), name='register_number'),
+    path('verify/', views.VerifyCode.as_view(), name='verify_code'),
 
 ]
--- smsotp/otp/views.py
+++ smsotp/otp/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import RegisterNumberForm
+import random
+from .models import OtpCode
+from django.contrib import messages
+from .utils import send_otp_code
 
 
 class HomeView(View):
@@ -13,4 +17,28 @@ class RegisterNumber(View):
     def get(self, request):
         form = RegisterNumberForm
         return render(request, 'otp/register_number.html', {'form': form})
+
+    def post(self, request):
+        form = RegisterNumberForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            random_code = random.randint(1000, 9999)
+            send_otp_code(cd['phone'], random_code)
+            OtpCode.objects.create(phone_number=cd['phone'], code=random_code)
+            messages.success(request, 'we sent you a code', 'success')
+            return redirect('otp:verify_code')
+
+
+class VerifyCode(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+
+
+
+
 
