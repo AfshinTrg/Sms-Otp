e67094ee632306ed6c2275434c0ccc50ff936a42
afshintrg <ataraghijoo@gmial.com>
2022-09-24 11:32:01 +0330

    Add VerifyCode view and form

--- smsotp/otp/forms.py
+++ smsotp/otp/forms.py
@@ -4,3 +4,6 @@ from django import forms
 class RegisterNumberForm(forms.Form):
     phone = forms.CharField(max_length=11)
 
+
+class VerifyCodeForm(forms.Form):
+    code = forms.IntegerField()
--- smsotp/otp/views.py
+++ smsotp/otp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import RegisterNumberForm
+from .forms import RegisterNumberForm, VerifyCodeForm
 import random
 from .models import OtpCode
 from django.contrib import messages
@@ -25,16 +25,34 @@ class RegisterNumber(View):
             random_code = random.randint(1000, 9999)
             send_otp_code(cd['phone'], random_code)
             OtpCode.objects.create(phone_number=cd['phone'], code=random_code)
+            request.session['phone_info'] = {
+                'phone_number': cd['phone'],
+            }
             messages.success(request, 'we sent you a code', 'success')
             return redirect('otp:verify_code')
 
 
 class VerifyCode(View):
     def get(self, request):
-        pass
+        form = VerifyCodeForm
+        return render(request, 'otp/verify_code.html', {'form': form})
 
     def post(self, request):
-        pass
+        user_session = request.session['phone_info']
+        code_instance = OtpCode.objects.get(phone_number=user_session['phone_number'])
+        form = VerifyCodeForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            if cd['code'] == code_instance.code:
+                messages.success(request, 'Your code is correct', 'success')
+                code_instance.delete()
+                return redirect('otp:home')
+            else:
+                messages.error(request, 'this code is wrong', 'danger')
+                return redirect('otp:verify_code')
+
+
+
 
 
 
